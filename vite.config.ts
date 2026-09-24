546c25b31c8d9c4e04a8ffb369ee06f7946dd7f3
Dwij Kansagara <dwijkansagara150911@gmail.com>
2026-09-24 21:07:14 +0530

    Fix Antideploy build and root asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
